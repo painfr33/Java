39a5344e8e3c0e1e0339c4b5536d93cc0120ba54
Bogdan <bogdan.chestikov@mail.ru>
2023-09-07 23:44:57 +0300

    Описание изменений

--- src/Zadacha6.java
+++ src/Zadacha6.java
@@ -1,5 +1,15 @@
 public class Zadacha6 {
-    public static void main(String[] arg){
-        ggfdjdgfkf
+    public static void main(String[] args) {
+        int n = 5;
+        int result = factorial(n);
+        System.out.printf("%d! = %d\n", n, result);
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++){
+            result *= i;
+        }
+    return result;
     }
 }
